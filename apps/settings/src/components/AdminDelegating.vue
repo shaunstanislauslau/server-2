e45d2acdcfce37809bd330395c6aae051fec0d1b
Carl Schwan <carl@carlschwan.eu>
2021-07-22 11:41:29 +0200

    Add admin delegation settings
    This makes it possible for selected groups to access some settings
    pages.
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

---
+++ apps/settings/src/components/AdminDelegating.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="adminrightsubgranting" class="section">
+		<h2>{{ t('settings', 'Admin Right Management') }}</h2>
+		<p class="settings-hint">
+			{{ t('settings', 'Here you can decide which group can access some of the admin settings.') }}
+		</p>
+
+		<div class="setting-list">
+			<div v-for="setting in availableSettings" :key="setting.class">
+				<h3>{{ setting.sectionName }}</h3>
+				<GroupSelect :groups="availableGroups" :initial-state="authorizedGroups" :setting="setting" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import GroupSelect from './AdminDelegation/GroupSelect'
+import { loadState } from '@nextcloud/initial-state'
+
+export default {
+	name: 'AdminDelegating',
+	components: {
+		GroupSelect,
+	},
+	data() {
+		const availableSettings = loadState('settings', 'available-settings')
+		const availableGroups = loadState('settings', 'available-groups')
+		const authorizedGroups = loadState('settings', 'authorized-groups')
+		// eslint-disable-next-line no-console
+		console.log('rere', authorizedGroups)
+		return {
+			availableSettings,
+			availableGroups,
+			authorizedGroups,
+		}
+	},
+	computed: {},
+	methods: {},
+}
+</script>
